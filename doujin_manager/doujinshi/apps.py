0faa2db2c3a0716d8690bdb46a66989bf682207c
Yoyo <b850108cd@gmail.com>
2023-03-23 22:25:25 +0800

    refactor: modify by black

--- doujin_manager/doujinshi/apps.py
+++ doujin_manager/doujinshi/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DoujinshiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'doujinshi'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "doujinshi"
