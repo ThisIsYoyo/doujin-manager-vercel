81fdd5ad860bd3552faa574f375c16de00e8e630
Yoyo <b850108cd@gmail.com>
2023-04-02 15:13:02 +0800

    fix(doujinshi/api-url): refine path name

--- doujin_manager/doujinshi/api_urls.py
+++ doujin_manager/doujinshi/api_urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     re_path(r"^author/list/", view=views.AuthorListView.as_view(), name="author list"),
     re_path(r"^doujinshi/list/", view=views.DoujinshiListView.as_view(), name="doujinshi list"),
     # get/modify single data
-    re_path(r"^circle/(?P<id>\d+)/", view=views.CircleGETView.as_view(), name="circle get by id"),
-    re_path(r"^author/(?P<id>\d+)/", view=views.AuthorGETView.as_view(), name="author get by id"),
-    re_path(r"^doujinshi/(?P<id>\d+)/", view=views.DoujinshiGETView.as_view(), name="doujinshi get by id"),
+    re_path(r"^circle/(?P<id>\d+)/", view=views.CircleGETView.as_view(), name="circle get/modify by id"),
+    re_path(r"^author/(?P<id>\d+)/", view=views.AuthorGETView.as_view(), name="author get/modify by id"),
+    re_path(r"^doujinshi/(?P<id>\d+)/", view=views.DoujinshiGETView.as_view(), name="doujinshi get/modify by id"),
     # create data
     re_path(r"^circle/", view=views.CircleCreateDestoryView.as_view(), name="circle create/delete"),
     re_path(r"^author/", view=views.AuthorCreateDestoryView.as_view(), name="author create/delete"),
